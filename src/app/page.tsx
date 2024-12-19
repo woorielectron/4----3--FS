229d59fc0f04345beffd8e747a703534749014aa
woorielectron <woorielectron@gmail.com>
2024-12-19 17:53:19 +0900

    forestOfstudy

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,22 @@
+import studyImage from '../image/root/공부의숲3x.png';
+
 import Image from "next/image";
 import styles from "./page.module.css";
+
 
 export default function Home()
 {
   return (
-    <>
-      <h1>공부의숲</h1>
-    </>
+    <div id={styles.IMain}>
+      <div className={styles.CNav}>
+        <div className={styles.CImage}>
+          <Image src={studyImage} alt='공부의숲' />
+        </div>
+        <div>
+          <input className={`${styles.CTitle} ${styles.CButtonFont}`} type='button' value="공부방 만들기" />
+        </div>
+      </div>
+    </div>
 
   );
 }
